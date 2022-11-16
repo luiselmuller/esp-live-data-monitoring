c3a65ffe55c62563c668a0ca29fef5058118bb80
luiselmuller <luiselmuller@gmail.com>
2022-11-16 17:02:42 -0400

    Updated device statistics example data

--- Web Interface/esp-monitor/src/pages/DeviceStatistics.tsx
+++ Web Interface/esp-monitor/src/pages/DeviceStatistics.tsx
@@ -2,6 +2,7 @@ import React, { FC, Suspense } from 'react'
 
 import EngineeringIcon from '@mui/icons-material/Engineering'; //temporary
 import { CircularProgress } from '@mui/material';
+import WifiIcon from '@mui/icons-material/Wifi'
 
 type deviceProps = {
   deviceData?: any,
@@ -36,6 +37,20 @@ const DeviceStatistics:FC<deviceProps> = (deviceData) => {
         <div className="flex w-full bg-secondary-dark-bg p-7 rounded-xl">
           <p className="flex w-full">Address</p>
           <p className="flex w-full">5C:BD:31:B6:51:B8</p>
+        </div>
+
+        <div className="flex w-full bg-secondary-dark-bg p-7 rounded-xl">
+          <p className="flex w-full">Signal</p>
+          <p className="flex w-full gap-2">
+            -54
+            <WifiIcon />  
+            {/* Change wifi icon according to signal strength */}
+          </p>
+        </div>
+
+        <div className="flex w-full bg-secondary-dark-bg p-7 rounded-xl">
+          <p className="flex w-full">Database</p>
+          <p className="flex w-full">Google Firestore</p>
         </div>
       </div>
     </div>
